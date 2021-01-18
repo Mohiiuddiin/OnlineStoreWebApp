13701abd1fa8d236bca727638b7327dba9378398
Mohiiuddiin <45856100+Mohiiuddiin@users.noreply.github.com>
2021-01-18 12:55:44 +0600

    Main

---
+++ OnlineStore.Core/Contracts/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using OnlineStore.Core.Models;
+using OnlineStore.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Contracts
+{
+    public interface IOrderService
+    {
+        void CreateOrder(Order baseOrder, List<BasketItemViewModel> basketItems);
+        List<Order> GetOrderList();
+        Order GetOrder(string Id);
+        void UpdateOrder(Order updatedOrder);
+    }
+}
---
+++ OnlineStore.Core/ViewModels/ProductManagerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using OnlineStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.ViewModels
+{
+    public class ProductManagerViewModel
+    {
+       public Product Product { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
